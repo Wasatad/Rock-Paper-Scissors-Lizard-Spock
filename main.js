896e477763a087507a84641571f97ff47bb2c5af
Nikolay Kuznetsov <n.smith@outlook.com>
2022-04-24 22:11:03 +0300

    fix rules

--- main.js
+++ main.js
@@ -470,14 +470,12 @@ document.querySelector(".close-bottom").addEventListener("click", hideRules);
 function showRules() {
   window.scrollTo(0, 0);
   document.body.style.overflow = "hidden";
-  document.body.style.maxHeight = "100vh";
 
   setTimeout(() => {
     document.querySelector(".rules").style.display = "flex";
     setTimeout(() => {
       document.querySelector(".rules-wrapper").style.transform =
         "translateY(0)";
-      window.scrollTo(0, 0);
     }, 400);
   }, 500);
 }
